8d2a3f69967a64cfdff99a0ae0856498bfbd2ee3
Dirkjan Ochtman <dirkjan@ochtman.nl>
2019-10-30 10:17:52 +0100

    Update bb8-postgres to async/await

--- postgres/examples/static_select.rs
+++ postgres/examples/static_select.rs
@@ -3,10 +3,6 @@ use tokio_postgres;
 
 use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
-use futures::{
-    future::{err, lazy, Either},
-    Future, Stream,
-};
 
 use std::str::FromStr;
 
@@ -14,37 +10,36 @@ use std::str::FromStr;
 //
 // The simplest way to start the db is using Docker:
 // docker run --name gotham-middleware-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
-fn main() {
+#[tokio::main]
+async fn main() {
     let config = tokio_postgres::config::Config::from_str(
         "postgresql://postgres:mysecretpassword@localhost:5432",
     )
     .unwrap();
     let pg_mgr = PostgresConnectionManager::new(config, tokio_postgres::NoTls);
 
-    tokio::run(lazy(|| {
-        Pool::builder()
-            .build(pg_mgr)
-            .map_err(|e| bb8::RunError::User(e))
-            .and_then(|pool| {
-                pool.run(|mut connection| {
-                    connection.prepare("SELECT 1").then(move |r| match r {
-                        Ok(select) => {
-                            let f = connection
-                                .query(&select, &[])
-                                .for_each(|row| {
-                                    println!("result: {}", row.get::<usize, i32>(0));
-                                    Ok(())
-                                })
-                                .then(move |r| match r {
-                                    Ok(_) => Ok(((), connection)),
-                                    Err(e) => Err((e, connection)),
-                                });
-                            Either::A(f)
-                        }
-                        Err(e) => Either::B(err((e, connection))),
-                    })
-                })
-            })
-            .map_err(|e| panic!("{:?}", e))
-    }));
+    let pool = match Pool::builder().build(pg_mgr).await {
+        Ok(pool) => pool,
+        Err(e) => panic!("builder error: {:?}", e),
+    };
+
+    let _ = pool
+        .run(|connection| {
+            async {
+                let select = match connection.prepare("SELECT 1").await {
+                    Ok(stmt) => stmt,
+                    Err(e) => return Err((e, connection)),
+                };
+
+                match connection.query_one(&select, &[]).await {
+                    Ok(row) => {
+                        println!("result: {}", row.get::<usize, i32>(0));
+                        Ok(((), connection))
+                    }
+                    Err(e) => Err((e, connection)),
+                }
+            }
+        })
+        .await
+        .map_err(|e| panic!("{:?}", e));
 }
