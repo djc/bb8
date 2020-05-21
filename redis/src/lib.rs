6d5d4dbe75361c4002fbe8e8cc399e88a24d40ea
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-05-21 14:31:05 +0200

    Remove obsolete Option wrapper for Redis connections

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -7,13 +7,13 @@
 //! use bb8_redis::{
 //!     bb8,
 //!     redis::{cmd, AsyncCommands},
-//!     RedisConnectionManager, RedisPool
+//!     RedisConnectionManager
 //! };
 //!
 //! #[tokio::main]
 //! async fn main() {
 //!     let manager = RedisConnectionManager::new("redis://localhost").unwrap();
-//!     let pool = RedisPool::new(bb8::Pool::builder().build(manager).await.unwrap());
+//!     let pool = bb8::Pool::builder().build(manager).await.unwrap();
 //!
 //!     let mut handles = vec![];
 //!
@@ -22,9 +22,8 @@
 //!
 //!         handles.push(tokio::spawn(async move {
 //!             let mut conn = pool.get().await.unwrap();
-//!             let conn = conn.as_mut().unwrap();
 //!
-//!             let reply: String = cmd("PING").query_async(conn).await.unwrap();
+//!             let reply: String = cmd("PING").query_async(&mut *conn).await.unwrap();
 //!
 //!             assert_eq!("PONG", reply);
 //!         }));
@@ -39,56 +38,9 @@
 pub use bb8;
 pub use redis;
 
-use futures::future::{Future, FutureExt};
-
 use async_trait::async_trait;
 use redis::aio::Connection;
 use redis::{Client, IntoConnectionInfo, RedisError};
-
-/// `RedisPool` is a convenience wrapper around `bb8::Pool` that hides the fact that
-/// `RedisConnectionManager` uses an `Option<Connection>` to smooth over the API incompatibility.
-#[derive(Debug, Clone)]
-pub struct RedisPool {
-    pool: bb8::Pool<RedisConnectionManager>,
-}
-
-impl RedisPool {
-    /// Constructs a new `RedisPool`, see the `bb8::Builder` documentation for description of
-    /// parameters.
-    pub fn new(pool: bb8::Pool<RedisConnectionManager>) -> RedisPool {
-        RedisPool { pool }
-    }
-
-    /// Access the `bb8::Pool` directly.
-    pub fn pool(&self) -> &bb8::Pool<RedisConnectionManager> {
-        &self.pool
-    }
-
-    /// Retrieve the pooled connection
-    pub async fn get(
-        &self,
-    ) -> Result<bb8::PooledConnection<'_, RedisConnectionManager>, bb8::RunError<RedisError>> {
-        self.pool().get().await
-    }
-
-    /// Run the function with a connection provided by the pool.
-    pub async fn run<'a, T, E, U, F>(&self, f: F) -> Result<T, bb8::RunError<E>>
-    where
-        F: FnOnce(Connection) -> U + Send + 'a,
-        U: Future<Output = Result<(Connection, T), E>> + Send + 'a,
-        E: From<<RedisConnectionManager as bb8::ManageConnection>::Error> + Send + 'a,
-        T: Send + 'a,
-    {
-        let f = move |conn: Option<Connection>| {
-            let conn = conn.unwrap();
-            f(conn).map(|res| match res {
-                Ok((conn, item)) => Ok((item, Some(conn))),
-                Err(err) => Err((err, None)),
-            })
-        };
-        self.pool.run(f).await
-    }
-}
 
 /// A `bb8::ManageConnection` for `redis::Client::get_async_connection`.
 #[derive(Clone, Debug)]
@@ -108,19 +60,18 @@ impl RedisConnectionManager {
 
 #[async_trait]
 impl bb8::ManageConnection for RedisConnectionManager {
-    type Connection = Option<Connection>;
+    type Connection = Connection;
     type Error = RedisError;
 
     async fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        self.client.get_async_connection().await.map(Some)
+        self.client.get_async_connection().await
     }
 
     async fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
-        // The connection should only be None after a failure.
-        redis::cmd("PING").query_async(conn.as_mut().unwrap()).await
+        redis::cmd("PING").query_async(conn).await
     }
 
-    fn has_broken(&self, conn: &mut Self::Connection) -> bool {
-        conn.is_none()
+    fn has_broken(&self, _: &mut Self::Connection) -> bool {
+        false
     }
 }
