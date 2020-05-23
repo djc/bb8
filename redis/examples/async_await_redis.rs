d69bcfc888d00c7831ec631a5e918581d8f04a5a
Anthony Mittaz <sync@me.com>
2020-05-23 16:15:05 +1000

    Add pubsub pool for redis

--- redis/examples/async_await_redis.rs
+++ redis/examples/async_await_redis.rs
@@ -1,4 +1,5 @@
 use bb8::{Pool, RunError};
+use futures_util::StreamExt as _;
 use redis::AsyncCommands;
 use redis::RedisError;
 
@@ -6,9 +7,10 @@ type RedisPool = Pool<bb8_redis::RedisConnectionManager>;
 
 // Select some data from Redis
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), RunError<RedisError>> {
     let redis_uri = String::from("redis://127.0.0.1:6379");
-    let manager = bb8_redis::RedisConnectionManager::new(redis_uri)
+
+    let manager = bb8_redis::RedisConnectionManager::new(redis_uri.clone())
         .expect("Unable to create redis connection manager");
 
     let pool = match Pool::builder().build(manager).await {
@@ -16,13 +18,41 @@ async fn main() {
         Err(e) => panic!("builder error: {:?}", e),
     };
 
-    let result = get_value(pool).await;
-    println!("Got result: {:?}", result);
+    let pubsub_manager = bb8_redis::RedisPubSubConnectionManager::new(redis_uri)
+        .expect("Unable to create redis connection manager");
+
+    let pubsub_pool = match Pool::builder().build(pubsub_manager).await {
+        Ok(pool) => pool,
+        Err(e) => panic!("pubsub builder error: {:?}", e),
+    };
+
+    let value = get_value(pool.clone()).await?;
+    println!("Got value: {:?}", value);
+
+    let mut pubsub_conn = pubsub_pool.get().await?;
+
+    pubsub_conn.subscribe("a-test-channel").await?;
+    let mut pubsub_stream = pubsub_conn.on_message();
+
+    publish(pool, "a-test-channel", "it works").await?;
+
+    let pubsub_msg: String = pubsub_stream.next().await.unwrap().get_payload()?;
+    println!("Got pubsub message: {:?}", pubsub_msg);
+
+    Ok(())
 }
 
 async fn get_value(pool: RedisPool) -> Result<Option<String>, RunError<RedisError>> {
     let mut connection = pool.get().await?;
 
-    let _ = connection.set::<&str, &str, ()>("my-key", "a value").await;
+    connection
+        .set::<&str, &str, ()>("my-key", "a value")
+        .await?;
     connection.get("my-key").await.map_err(RunError::User)
+}
+
+async fn publish(pool: RedisPool, channel: &str, value: &str) -> Result<(), RunError<RedisError>> {
+    let mut conn = pool.get().await?;
+
+    conn.publish(channel, value).await.map_err(RunError::User)
 }
--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -39,7 +39,7 @@ pub use bb8;
 pub use redis;
 
 use async_trait::async_trait;
-use redis::aio::Connection;
+use redis::aio::{Connection, PubSub};
 use redis::{Client, IntoConnectionInfo, RedisError};
 
 /// A `bb8::ManageConnection` for `redis::Client::get_async_connection`.
@@ -69,6 +69,45 @@ impl bb8::ManageConnection for RedisConnectionManager {
 
     async fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
         redis::cmd("PING").query_async(conn).await
+    }
+
+    fn has_broken(&self, _: &mut Self::Connection) -> bool {
+        false
+    }
+}
+
+/// A `bb8::ManageConnection` for `redis::Client::get_async_connection`.
+#[derive(Clone, Debug)]
+pub struct RedisPubSubConnectionManager {
+    client: Client,
+}
+
+impl RedisPubSubConnectionManager {
+    /// Create a new `RedisPubSubConnectionManager`.
+    /// See `redis::Client::open` for a description of the parameter types.
+    pub fn new<T: IntoConnectionInfo>(info: T) -> Result<RedisPubSubConnectionManager, RedisError> {
+        Ok(RedisPubSubConnectionManager {
+            client: Client::open(info.into_connection_info()?)?,
+        })
+    }
+}
+
+#[async_trait]
+impl bb8::ManageConnection for RedisPubSubConnectionManager {
+    type Connection = PubSub;
+    type Error = RedisError;
+
+    async fn connect(&self) -> Result<Self::Connection, Self::Error> {
+        self.client
+            .get_async_connection()
+            .await
+            .map(|conn| conn.into_pubsub())
+    }
+
+    async fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
+        let test_sub_name = "__test__-sub-from-bb8";
+        conn.subscribe(test_sub_name).await?;
+        conn.unsubscribe(test_sub_name).await
     }
 
     fn has_broken(&self, _: &mut Self::Connection) -> bool {
