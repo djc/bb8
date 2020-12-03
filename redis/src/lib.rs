9afe8b42b8e498d066cecfbda43058242aec15cd
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-12-03 22:53:08 +0100

    Revert "Temporarily remove redis crate"
    This reverts commit e8c17961ebb11550c634be082355af991d61d8f8.

---
+++ redis/src/lib.rs
@@ -0,0 +1,82 @@
+//! Redis support for the `bb8` connection pool.
+//!
+//! # Example
+//!
+//! ```
+//! use futures_util::future::join_all;
+//! use bb8_redis::{
+//!     bb8,
+//!     redis::{cmd, AsyncCommands},
+//!     RedisConnectionManager
+//! };
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+//!     let pool = bb8::Pool::builder().build(manager).await.unwrap();
+//!
+//!     let mut handles = vec![];
+//!
+//!     for _i in 0..10 {
+//!         let pool = pool.clone();
+//!
+//!         handles.push(tokio::spawn(async move {
+//!             let mut conn = pool.get().await.unwrap();
+//!
+//!             let reply: String = cmd("PING").query_async(&mut *conn).await.unwrap();
+//!
+//!             assert_eq!("PONG", reply);
+//!         }));
+//!     }
+//!
+//!     join_all(handles).await;
+//! }
+//! ```
+#![allow(clippy::needless_doctest_main)]
+#![deny(missing_docs, missing_debug_implementations)]
+
+use std::ops::DerefMut;
+
+pub use bb8;
+pub use redis;
+
+use async_trait::async_trait;
+use redis::aio::Connection;
+use redis::{Client, IntoConnectionInfo, RedisError};
+
+/// A `bb8::ManageConnection` for `redis::Client::get_async_connection`.
+#[derive(Clone, Debug)]
+pub struct RedisConnectionManager {
+    client: Client,
+}
+
+impl RedisConnectionManager {
+    /// Create a new `RedisConnectionManager`.
+    /// See `redis::Client::open` for a description of the parameter types.
+    pub fn new<T: IntoConnectionInfo>(info: T) -> Result<RedisConnectionManager, RedisError> {
+        Ok(RedisConnectionManager {
+            client: Client::open(info.into_connection_info()?)?,
+        })
+    }
+}
+
+#[async_trait]
+impl bb8::ManageConnection for RedisConnectionManager {
+    type Connection = Connection;
+    type Error = RedisError;
+
+    async fn connect(&self) -> Result<Self::Connection, Self::Error> {
+        self.client.get_async_connection().await
+    }
+
+    async fn is_valid(
+        &self,
+        conn: &mut bb8::PooledConnection<'_, Self>,
+    ) -> Result<(), Self::Error> {
+        redis::cmd("PING").query_async(conn.deref_mut()).await
+    }
+
+    fn has_broken(&self, _: &mut Self::Connection) -> bool {
+        false
+    }
+}
