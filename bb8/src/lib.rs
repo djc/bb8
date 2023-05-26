cc065bd5656a718cd10fcb04533728fb90f15838
Zain Kabani <zain.kabani@instacart.com>
2023-05-26 13:47:39 -0400

    Add queue strategy to lib

--- bb8/src/lib.rs
+++ bb8/src/lib.rs
@@ -36,7 +36,7 @@
 mod api;
 pub use api::{
     Builder, CustomizeConnection, ErrorSink, ManageConnection, NopErrorSink, Pool,
-    PooledConnection, RunError, State,
+    PooledConnection, QueueStrategy, RunError, State,
 };
 
 mod inner;
