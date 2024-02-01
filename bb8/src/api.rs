djc/bb8

commit 0dca340f89e1d3201eb8abfede040051e3214028

Remove PoolInner::get_owned()

bb8/src/api.rs
@@ -60,7 +60,10 @@ impl<M: ManageConnection> Pool<M> {
     /// Using an owning `PooledConnection` makes it easier to leak the connection pool. Therefore, [`Pool::get`]
     /// (which stores a lifetime-bound reference to the pool) should be preferred whenever possible.
     pub async fn get_owned(&self) -> Result<PooledConnection<'static, M>, RunError<M::Error>> {
-        self.inner.get_owned().await
+        Ok(PooledConnection {
+            conn: self.get().await?.take(),
+            pool: Cow::Owned(self.inner.clone()),
+        })
     }
 
     /// Get a new dedicated connection that will not be managed by the pool.
@@ -385,17 +388,9 @@ where
     pub(crate) fn drop_invalid(mut self) {
         let _ = self.conn.take();
     }
-}
 
-impl<M> PooledConnection<'static, M>
-where
-    M: ManageConnection,
-{
-    pub(crate) fn new_owned(pool: PoolInner<M>, conn: Conn<M::Connection>) -> Self {
-        Self {
-            pool: Cow::Owned(pool),
-            conn: Some(conn),
-        }
+    pub(crate) fn take(&mut self) -> Option<Conn<M::Connection>> {
+        self.conn.take()
     }
 }
 

bb8/src/inner.rs
@@ -89,22 +89,6 @@ where
         }
     }
 
-    pub(crate) async fn get_owned(
-        &self,
-    ) -> Result<PooledConnection<'static, M>, RunError<M::Error>> {
-        let future = self.make_pooled(|this, conn| {
-            let pool = PoolInner {
-                inner: Arc::clone(&this.inner),
-            };
-            PooledConnection::new_owned(pool, conn)
-        });
-
-        match timeout(self.inner.statics.connection_timeout, future).await {
-            Ok(result) => result,
-            _ => Err(RunError::TimedOut),
-        }
-    }
-
     pub(crate) async fn make_pooled<'a, 'b>(
         &'a self,
         make_pooled_conn: impl Fn(&'a Self, Conn<M::Connection>) -> PooledConnection<'b, M>,

commit 151a26055795a317ee5e4de999676bdaf1ae5d96

bb8: track connection state explicitly
0dca340f89e1d3201eb8abfede040051e3214028 caused a regression where the
library would update the pool state to mark a connection as lost because
we disconnected the underlying connection from the `PooledConnection`.
Instead, add a `state` field to `PooledConnection` that tracks the
state of the `conn` field, so we can do the right thing on drop.

bb8/src/api.rs
@@ -63,6 +63,7 @@ impl<M: ManageConnection> Pool<M> {
         Ok(PooledConnection {
             conn: self.get().await?.take(),
             pool: Cow::Owned(self.inner.clone()),
+            state: ConnectionState::Present,
         })
     }
 
@@ -372,6 +373,7 @@ where
 {
     pool: Cow<'a, PoolInner<M>>,
     conn: Option<Conn<M::Connection>>,
+    pub(crate) state: ConnectionState,
 }
 
 impl<'a, M> PooledConnection<'a, M>
@@ -382,14 +384,12 @@ where
         Self {
             pool: Cow::Borrowed(pool),
             conn: Some(conn),
+            state: ConnectionState::Present,
         }
     }
 
-    pub(crate) fn drop_invalid(mut self) {
-        let _ = self.conn.take();
-    }
-
-    pub(crate) fn take(&mut self) -> Option<Conn<M::Connection>> {
+    pub(crate) fn take(mut self) -> Option<Conn<M::Connection>> {
+        self.state = ConnectionState::Extracted;
         self.conn.take()
     }
 }
@@ -429,8 +429,22 @@ where
     M: ManageConnection,
 {
     fn drop(&mut self) {
-        self.pool.as_ref().put_back(self.conn.take());
+        if let ConnectionState::Extracted = self.state {
+            return;
+        }
+
+        debug_assert!(self.conn.is_some(), "incorrect state {:?}", self.state);
+        if let Some(conn) = self.conn.take() {
+            self.pool.as_ref().put_back(conn, self.state);
+        }
     }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) enum ConnectionState {
+    Present,
+    Extracted,
+    Invalid,
 }
 
 /// bb8's error type.

bb8/src/inner.rs
@@ -9,7 +9,7 @@ use futures_util::TryFutureExt;
 use tokio::spawn;
 use tokio::time::{interval_at, sleep, timeout, Interval};
 
-use crate::api::{Builder, ManageConnection, PooledConnection, RunError};
+use crate::api::{Builder, ConnectionState, ManageConnection, PooledConnection, RunError};
 use crate::internals::{Approval, ApprovalIter, Conn, SharedPool, State};
 
 pub(crate) struct PoolInner<M>
@@ -109,7 +109,7 @@ where
                     Ok(()) => return Ok(conn),
                     Err(e) => {
                         self.inner.forward_error(e);
-                        conn.drop_invalid();
+                        conn.state = ConnectionState::Invalid;
                         continue;
                     }
                 }
@@ -129,19 +129,16 @@ where
     }
 
     /// Return connection back in to the pool
-    pub(crate) fn put_back(&self, conn: Option<Conn<M::Connection>>) {
-        let conn = conn.and_then(|mut conn| {
-            if !self.inner.manager.has_broken(&mut conn.conn) {
-                Some(conn)
-            } else {
-                None
-            }
-        });
+    pub(crate) fn put_back(&self, mut conn: Conn<M::Connection>, state: ConnectionState) {
+        debug_assert!(
+            !matches!(state, ConnectionState::Extracted),
+            "handled in caller"
+        );
 
         let mut locked = self.inner.internals.lock();
-        match conn {
-            Some(conn) => locked.put(conn, None, self.inner.clone()),
-            None => {
+        match (state, self.inner.manager.has_broken(&mut conn.conn)) {
+            (ConnectionState::Present, false) => locked.put(conn, None, self.inner.clone()),
+            (_, _) => {
                 let approvals = locked.dropped(1, &self.inner.statics);
                 self.spawn_replenishing_approvals(approvals);
             }